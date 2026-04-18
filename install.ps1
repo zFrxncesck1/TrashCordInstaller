c1220d8b7d88681c061686e325b4bb72cc172e13
zFry <99491170+zFrxncesck1@users.noreply.github.com>
2026-04-18 02:05:22 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
-$link = "https://github.com/Equicord/Equilotl/releases/latest/download/EquilotlCli.exe"
+$link = "https://github.com/zFrxncesck1/TrashCordInstaller/releases/latest/download/TrashCordInstallerCli.exe"
 
-$outfile = "$env:TEMP\EquilotlCli.exe"
+$outfile = "$env:TEMP\TrashCordInstallerCli.exe"
 
 Write-Output "Downloading installer to $outfile"
 
